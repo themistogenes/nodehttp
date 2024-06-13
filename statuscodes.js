479d2b777877f3efa55eab72f053a483b8bdab05
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-06-13 19:10:49 -0400

    status codes

---
+++ statuscodes.js
@@ -0,0 +1,16 @@
+// 1.xx INFORMATIONAL
+
+// 2.xx SUCCESS
+// 200  Success
+// 201  Created
+// 204  No Content
+
+// 3.xx REDIRECTION
+
+// 4.xx CLIENT ERRORS
+// 400  Bad Request
+// 401  Unauthorized
+// 404  Not Found
+
+// 5.xx SERVER ERRORS
+// 500  Internal Server Error
